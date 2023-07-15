ffd2baf56d7c292184e1b2476dfa3de183450973
Eduardo Sousa Nunes <devedu2004@gmail.com>
2023-07-14 21:56:12 -0300

    add mesas

---
+++ site-restaurante/pedidos/openmesa/mesa02/script.js
@@ -0,0 +1,7 @@
+var logo = document.getElementById("logo01")
+function mudcolor(){
+logo.setAttribute("src","../../../imgs/logo_vermelha_home.png")
+}
+function returncolor(){
+    logo.setAttribute("src","../../../imgs/logo_branca_home.png")
+}
